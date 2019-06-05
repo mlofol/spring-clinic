b9a6eb7fe704f771466de296e2e0015777ffce66
Carmen Lopez <carmen.lopez@gmail.com>
2019-06-05 14:07:30 +0200

    Initial commit

---
+++ src/test/java/springframework/springclinic/SpringClinicApplicationTests.java
@@ -0,0 +1,16 @@
+package springframework.springclinic;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringClinicApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
